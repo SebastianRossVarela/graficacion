6183e350c62166e5db08bf5f627165f8b6a68122
Sebastian Ross Varela <l24170723@culiacan.tecnm.mx>
2026-02-16 17:55:00 +0000

    co

--- 01_lineas/sketch.js
+++ 01_lineas/sketch.js
@@ -3,5 +3,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+stroke(0);
+line(30, 20, 185, 20);
+stroke(126);
+line(185, 20, 185, 175);
+stroke(255);
+line(185, 175, 30, 20);
+
+stroke(0);
+line(615, 20, 770, 20);
+stroke(126);
+line(770, 20, 770,175);
+stroke(255);
+line(770, 175, 615, 175);
+line(615, 175, 615, 20);
+
+
+  
 }
